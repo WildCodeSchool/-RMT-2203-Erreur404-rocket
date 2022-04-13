1a3bc81b995e2b8f4cae97486e65f3d4970e8c51
Alexandre Genin <alexandre.genin.pro@gmail.com>
2022-04-13 15:59:12 +0200

    minor modif

--- frontend/src/pages/Accueil.jsx
+++ frontend/src/pages/Accueil.jsx
@@ -5,8 +5,6 @@ function Acceuil() {
   return (
     <div>
       <h1 className="colortest">Acceuil</h1>
-      <h2 className="colortest1">bonjour</h2>
-      <h3 className="colortest2">enrevoir</h3>
     </div>
   );
 }
