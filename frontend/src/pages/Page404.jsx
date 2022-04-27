0b421f6c018c8f9ceb6e26d60917ecf5e28f76f5
Mariakarara <claind15@yahoo.fr>
2022-04-27 12:24:37 +0200

    correction import

--- frontend/src/pages/Page404.jsx
+++ frontend/src/pages/Page404.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
+import rocketmongolfiere from "../assets/rocket-mongolfiere.jpg";
+
+import "../styles/Page404.css";
+
 function Page404() {
   return (
-    <div>
-      <h1>Page404</h1>
+    <div className="maincontainer">
+      <div className="section1">
+        <h1>Error</h1>
+
+        <p>
+          4<span>a</span>4
+        </p>
+        <h2>Oups! La Team Rocket revient</h2>
+      </div>
+
+      <div className="rocket-mongolfiere">
+        <img
+          className="mongolfiere"
+          src={rocketmongolfiere}
+          alt="rocketmongolfiere"
+        />
+      </div>
     </div>
   );
 }
