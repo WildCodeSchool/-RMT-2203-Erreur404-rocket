89f2e66417c237fee90d1aef9fe84236b6f66fcf
limchristian81@gmail.com <limchristian81@gmail.com>
2022-05-04 17:14:16 +0200

    Responsive design OK et erreurs corrigées

--- frontend/src/components/DescriptionCard.jsx
+++ frontend/src/components/DescriptionCard.jsx
@@ -1,18 +1,93 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+import ProgressBar from "./ProgressBar";
+import teamRocketball from "../assets/team-rocket ball.png";
+
+import "../styles/PokemonDescription.css";
 
 function DescriptionCard({ detail }) {
   return (
-    <div>
-      <h1>{detail.name}</h1>
-      <h2># {detail.id}</h2>
-      <h2>Weight : {detail.weight}</h2>
-      <h2>Height : {detail.height}</h2>
-      <h2>Type : {detail.types && detail.types[0].type.name}</h2>
-      <img
-        className="pkm-picture"
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${detail.id}.png`}
-        alt={`Official ${detail.name} artwork`}
-      />
+    <div className="pkm-data">
+      <div className="main-data">
+        <h1>{detail.name}</h1>
+        <div className="main-data-detail">
+          <p>
+            ID :<br /> #{detail.id}{" "}
+          </p>
+          <p>
+            Weight :<br />
+            {detail.weight / 10} kgs
+          </p>
+          <p>
+            height :<br />
+            {detail.height / 10} m
+          </p>
+          <p>
+            Type 1 :<br />
+            {detail.types && detail.types[0].type.name}
+          </p>
+          {detail.types && detail.types[1] ? (
+            <p>Type 2 : {detail.types[1].type.name}</p>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+
+      <div className="box-pkm-picture-basic-stats">
+        <img
+          className="pkm-picture"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${detail.id}.png`}
+          alt={`Official ${detail.name} artwork`}
+        />
+
+        <div className="pkm-basic-stats">
+          {/* <h2>STATISTIQUES</h2> */}
+          {/* detail.stats et vide au moment du useeffect , donc au moment de la reactualisation, il plante car il ne peut pas lancer son map* Du coup  il faut  le && : */}
+          {detail.stats &&
+            detail.stats.map((el) => (
+              <ul className="stats-name">
+                {el.stat.name}
+                <li className="stats-value">
+                  <ProgressBar progress={el.base_stat} />
+                </li>
+              </ul>
+            ))}
+        </div>
+      </div>
+      <div className="btn-ajouter-au-panier">
+        <img
+          className="rocketball-descriptioncard"
+          src={teamRocketball}
+          alt="rocketball"
+        />
+        <NavLink to="/home">
+          Attrape ce Pokémon
+          <br />
+          et ajoute le au panier
+        </NavLink>
+      </div>
+
+      {/* ---------- Section separateur ---------- */}
+
+      {/* <div className="separation-descriptioncard">
+        <hr className="separateur-descriptioncard" />
+          <img className="rocketball-descriptioncard" src={teamRocketball} alt="rocketball" />
+        <hr className="separateur-descriptioncard" />
+      </div> */}
+
+      {/* ---------- Section Details ---------- */}
+
+      {/* on rajoute un div pour les Capacités du pokémon dans lequel on mappe à nouveau */}
+
+      <ul className="abilities">
+        <h2>Capacités spéciales :</h2>
+        {detail.abilities &&
+          detail.abilities.map((el) => (
+            <li className="abilities-name">{el.ability.name}</li>
+          ))}
+      </ul>
     </div>
   );
 }
--- frontend/src/pages/PokemonDescription.jsx
+++ frontend/src/pages/PokemonDescription.jsx
@@ -1,25 +1,38 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 
-import DescriptionCard from "@components/DescriptionCard";
+import PokemonDetail from "@components/PokemonDetail";
+import DescriptionCard from "../components/DescriptionCard";
 
-const pokemonDescription = () => {
-  const { pokedexnum } = useParams();
-  const [detail, setDetail] = useState({});
+import "../styles/PokemonDescription.css";
+
+function PokemonDescription() {
+  const { pokedexnum } = useParams(); // on declare les variables de l'url dynamique du lien dans pokedex ICI.
+  const [detail, setDetail] = useState({}); // feetch de l'api pour chaque pokemon : ici ce n'est pas un tableau vide mais un objet vide.
 
   useEffect(() => {
     axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${pokedexnum}`)
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokedexnum}`) // on fetch uniquement les pokémon qui correspondent au bouton sur lequel on a cliqué,  dans pokedex , en ajoutant le useparam.
+
       .then((res) => setDetail(res.data));
   }, []);
-
   return (
     <div>
       <DescriptionCard detail={detail} />
-      {/* <PokemonDetail /> */}
+      <PokemonDetail />
+      <div className="box-btn-home-panier">
+        <NavLink to="/home" className="btn-pkm-home">
+          Retourner au Pokedex
+        </NavLink>
+        {/* <NavLink to={`/home`} >
+        <button className="btn-pkm-panier">
+            Ajouter au panier
+        </button>
+        </NavLink> */}
+      </div>
     </div>
   );
-};
+}
 
-export default pokemonDescription;
+export default PokemonDescription;
