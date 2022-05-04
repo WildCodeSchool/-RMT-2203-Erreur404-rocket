7d9cd690f3edf19b0a6d283d98b6568204fcc1a1
KevinLavigne <lavigne.kevin02@gmail.com>
2022-05-04 16:33:57 +0200

    creation du css complet de la page carte en desktop et responsive(valable max 640px car react-img-mapper prend des valeur en px obligatoirement pour eviter la déformation des zones)

--- frontend/src/pages/Carte.jsx
+++ frontend/src/pages/Carte.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import Mapper from "@components/Mapper";
 
+import pokemonSelected from "../data/pokemonSelected";
+import MapCard from "../components/MapCard";
+import teamRocketball from "../assets/team-rocket ball.png";
+import jessie from "../assets/jessie-alone2.png";
+import james from "../assets/james-alone.png";
+
 import "../styles/Carte.css";
 
 function Carte() {
   return (
-    <div className="mapContainer">
-      <h1>Carte</h1>
-      <Mapper />
+    <div>
+      <div className="mapContainer">
+        <h1>Carte</h1>
+
+        <div className="separation">
+          <hr className="separateur" />
+          <img className="rocketball" src={teamRocketball} alt="rocketball" />
+          <hr className="separateur" />
+        </div>
+
+        <div className="flex-map-container">
+          <div className="img-flex">
+            <img className="james-alone" src={james} alt="James" />
+            <img className="jessie-alone" src={jessie} alt="James" />
+            <Mapper />
+          </div>
+          <div className="map-pokemon-container">
+            {pokemonSelected.map((pokemon) => (
+              <MapCard pokemon={pokemon} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
